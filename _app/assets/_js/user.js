094b929fbb1a7a03133285bfff2e67e68c493011
highandmighty <arseny@softculture.cc>
2020-03-22 22:27:37 +0300

    CSS margins fixed

--- _app/assets/_js/user.js
+++ _app/assets/_js/user.js
@@ -1,1 +1,17 @@
 // Custom user scripts
+function linksTargetBlank() {
+  // remove subdomain of current site's url and setup regex
+  var internal = location.host.replace("www.", "");
+      internal = new RegExp(internal, "i");
+
+  var a = document.getElementsByTagName('a'); // then, grab every link on the page
+  for (var i = 0; i < a.length; i++) {
+    var href = a[i].host; // set the host of each link
+    if( !internal.test(href) ) { // make sure the href doesn't contain current site's host
+      a[i].setAttribute('target', '_blank'); // if it doesn't, set attributes
+      console.log(i);
+    }
+  }
+};
+
+linksTargetBlank();
